db2ebb9aa4c41adf14ea2264f23e5eb0e46af8e9
mathletedev <mathletedev@gmail.com>
2021-06-14 11:52:28 -0700

    Add header details

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import React, { FC, ReactChild } from "react";
+import { __baseURL__ } from "../lib/constants";
 import styles from "../styles/layout.module.css";
 
 interface Props {
@@ -8,17 +9,26 @@ interface Props {
 	title?: string;
 }
 
-const Layout: FC<Props> = ({ children, title = "MathleteDev" }) => {
+const Layout: FC<Props> = ({ children, title }) => {
 	return (
 		<div>
 			<Head>
-				<title>{title}</title>
+				<title>{title ? `${title} • MathleteDev` : "MathleteDev"}</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 				<link
 					href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap"
 					rel="stylesheet"
 				/>
-				<link href="/img/favicon.png" rel="icon" />
+				<link href="/icons/1024x1024.png" rel="icon" />
+				<meta name="description" content="My personal website" />
+				<meta name="author" content="Neal Wang" />
+				<meta name="keywords" content="MathleteDev" />
+				<meta name="og:title" content={title || "MathleteDev"} />
+				<meta name="og:description" content="My personal website" />
+				<meta name="og:site_name" content="MathleteDev" />
+				<meta name="og:image" content={`${__baseURL__}/icons/256x256.png}`} />
+				<meta charSet="utf-8" />
+				<link rel="manifest" href="/manifest.json" />
 			</Head>
 			<nav id={styles["nav-bar"]}>
 				<Link href="/">
---
+++ src/lib/constants.ts
@@ -0,0 +1,2 @@
+export const __websiteURL__ = "https://mathletedev.vercel.app";
+export const __baseURL__ = process.env.NEXT_PUBLIC_BASE_URL || __websiteURL__;
