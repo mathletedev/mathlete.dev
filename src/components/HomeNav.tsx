39336104af6dcf7586b38426bfc2c0480070e7a5
mathletedev <mathletedev@gmail.com>
2021-06-16 09:29:50 -0700

    use react-icons instead of material-ui

--- src/components/HomeNav.tsx
+++ src/components/HomeNav.tsx
@@ -1,19 +1,34 @@
-import { More } from "@material-ui/icons";
+import { useRouter } from "next/router";
 import React, { FC } from "react";
+import { MdMore } from "react-icons/md";
 import { __homePages__ } from "../lib/constants";
 import styles from "../styles/homenav.module.css";
 
 const HomeNav: FC = () => {
+	const router = useRouter();
+
 	return (
 		<div id={styles["nav-drawer"]}>
-			{Object.keys(__homePages__).map((path) => (
-				<a href={`#${path}`} className={styles["nav-button-link"]} key={path}>
-					{__homePages__[path]}
-				</a>
-			))}
-			<a href="/more" className={styles["nav-button-link"]} key="more">
-				<More className={styles["nav-button"]} />
-			</a>
+			{Object.keys(__homePages__).map((path) => {
+				const Icon = __homePages__[path];
+
+				return (
+					<button
+						onClick={() => router.push(`#${path}`)}
+						className={styles["nav-button-link"]}
+						key={path}
+					>
+						<Icon className={styles["nav-button"]} />
+					</button>
+				);
+			})}
+			<button
+				onClick={() => router.push("/more")}
+				className={styles["nav-button-link"]}
+				key="more"
+			>
+				<MdMore className={styles["nav-button"]} />
+			</button>
 		</div>
 	);
 };
