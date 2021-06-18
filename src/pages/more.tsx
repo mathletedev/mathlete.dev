5eb9f8376eef18afddb7f86f0f4bb228f7f0681d
mathletedev <mathletedev@gmail.com>
2021-06-17 20:46:36 -0700

    hide overflow

--- src/pages/more.tsx
+++ src/pages/more.tsx
@@ -1,9 +1,9 @@
-import React, { FC, Fragment } from "react";
-import styles from "../styles/about.module.css";
+import React, { FC } from "react";
+import styles from "../styles/more.module.css";
 
 const Rickroll: FC = () => {
 	return (
-		<Fragment>
+		<div id={styles["no-overflow"]}>
 			<div id={styles.blocker}></div>
 			<iframe
 				src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1&controls=0"
@@ -12,7 +12,7 @@ const Rickroll: FC = () => {
 				frameBorder={0}
 				id={styles.video}
 			></iframe>
-		</Fragment>
+		</div>
 	);
 };
 
